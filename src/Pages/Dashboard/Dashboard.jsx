9d2aed32decf0dbaad48bac73f66556af66c0cd7
bilal-azfar-x <azfarmbilal10@gmail.com>
2025-01-19 13:51:45 +0500

    Removing attendance_performance chart and adding student_performance finalised chart.

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import './Components/Attendance Summary/Attendance_Summary';
-import Attendance_Summary from "./Components/Attendance Summary/Attendance_Summary";
+import '../Dashboard/Dashboard.css';
+import Student_Performance from "./Components/Student Performance/Student_Performance";
 import Announcement from "./Components/Announcements/Announcements";
 import ChatList from "./Components/Student Chats/Student_Chats";
 
 
 function Dashboard () {
     return (
-        <div>
-            <Announcement />
-            <ChatList />
-            <Attendance_Summary />
+        <div className="components-div">
+            <Student_Performance />
+            {/*  <ChatList />  */}
+            {/*  <Announcement />  */}
         </div>
     );
 }
